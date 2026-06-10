b91f8630cdebdd7749eb944374c59a9310c20070
carpaltunnel <carpaltunnel-email>
2026-06-09 21:03:27 -0500

    chicken details component with static (ts) data

--- src/app/chicken-details/chicken-details.ts
+++ src/app/chicken-details/chicken-details.ts
@@ -9,4 +9,12 @@ import { Component, signal } from '@angular/core';
 export class ChickenDetails {
   protected readonly welcomeMessage = signal('Welcome to chicken-details component!');
 
+  protected readonly currentChicken = {
+    id: '75974d26-bd05-4d9f-9dc6-7ce8413801aa',
+    name: 'Chipotle',
+    breed: 'Buff Orpington',
+    weight: 3.1,
+    color: 'Orange'
+  };
+
 }
