5111e795a1427e5d87cda1c38bffddb1513b3dec
carpaltunnel <carpaltunnel-email>
2026-06-11 20:53:23 -0500

    incomplete chicken-edit component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ChickenListing } from './chicken-listing/chicken-listing.js';
 import { ChickenDetails } from './chicken-details/chicken-details.js';
+import { ChickenEdit } from './chicken-edit/chicken-edit.js';
 
 export const routes: Routes = [
   {
@@ -12,5 +13,10 @@ export const routes: Routes = [
     path: 'chickens/:id',
     component: ChickenDetails,
     title: 'Chicken Details'
+  },
+  {
+    path: 'chickens/edit/:id',
+    component: ChickenEdit,
+    title: 'Chicken Edit'
   }
 ];
