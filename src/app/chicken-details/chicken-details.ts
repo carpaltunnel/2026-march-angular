108ffc660006f2dea2deb4ff4c359d78a6461289
carpaltunnel <carpaltunnel-email>
2026-06-11 20:06:59 -0500

    Wire chicken details to find/render chicken data based on the ActivatedRoute/id"

--- src/app/chicken-details/chicken-details.ts
+++ src/app/chicken-details/chicken-details.ts
@@ -1,15 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { Chicken } from '../types/chicken.js';
 import { CHICKEN } from '../mock-data/mock-chicken.js';
+import { CHICKENS } from '../mock-data/mock-chickens.js';
 
 @Component({
   selector: 'app-chicken-details',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './chicken-details.html',
   styleUrl: './chicken-details.css',
 })
 export class ChickenDetails {
-  protected readonly welcomeMessage = signal('Welcome to chicken-details component!');
+  route: ActivatedRoute = inject(ActivatedRoute);
+  chickenId: string;
+  protected currentChicken: Chicken;
 
-  protected readonly currentChicken = CHICKEN;
+  constructor() {
+    this.chickenId = this.route.snapshot.params['id'];
+    this.currentChicken = CHICKENS.find(c => c.id === this.chickenId) || CHICKEN;
+  }
+
+  
 
 }
