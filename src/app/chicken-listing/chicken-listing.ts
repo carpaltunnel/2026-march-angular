4c71d4b3acdf8aef659f00737abe1f4415fcd047
carpaltunnel <carpaltunnel-email>
2026-06-16 19:30:48 -0500

    add new chicken link to listing page and nav bar

--- src/app/chicken-listing/chicken-listing.ts
+++ src/app/chicken-listing/chicken-listing.ts
@@ -1,12 +1,14 @@
 import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { Chicken } from '../types/chicken.js';
 import { CHICKENS } from '../mock-data/mock-chickens.js';
 import { ChickenOverview } from '../chicken-overview/chicken-overview.js';
 import { ChickensService } from '../chickens.service.js';
 
+
 @Component({
   selector: 'app-chicken-listing',
-  imports: [ChickenOverview],
+  imports: [ChickenOverview, RouterModule],
   templateUrl: './chicken-listing.html',
   styleUrl: './chicken-listing.css',
 })
